0fe7de82935ed9663a15f3f779e2ac92ad343519
Alex Sweeney <alexsweeney@live.co.uk>
2020-04-10 19:51:49 +0700

    initial commit

---
+++ src/Metronome.jsx
@@ -0,0 +1,9 @@
+import React from 'react'; 
+
+const Metronome = () => {
+	return (
+		<h1> Metronome </h1>
+	)
+}
+
+export default Metronome;
---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Metronome from "./Metronome.jsx";
+
+var root = document.querySelector("#root");
+
+ReactDOM.render(
+	<>
+		<Metronome/>
+	</>,
+	root
+);
