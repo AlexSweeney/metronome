e240e7faebb3b6bc45ef981c7ceee1e47b978b02
Alex Sweeney <alexsweeney@live.co.uk>
2020-04-14 00:18:51 +0700

    refactor(metronome.jsx) useEffect for metronome click, removed mut button

--- src/Metronome.jsx
+++ src/Metronome.jsx
@@ -1,5 +1,5 @@
 /*
-    fix: mutliple click play
+    refactor => useEffect hook for timer
 
     keep playing when input new time
 
@@ -27,48 +27,73 @@ import Wood from './audio/wood.mp3';
 
 const Metronome = () => {
     console.log('render');
-    let [BPM, changeBPM] = React.useState(40);  
-    let [playMode, changePlayMode] = React.useState('stop');
-    let clickTime = getClickTime(BPM);    
+    let [BPM, setBPM] = React.useState(40);  
+    let [playMode, setPlayMode] = React.useState('stop');
+    let clickTime = getClickTime(BPM);   
+
+    console.log('BPM', BPM); 
 
     let woodAudio = document.createElement('audio');
     woodAudio.src = Wood; 
 
+    React.useEffect(() => {
+        console.log('useEffect called', this);
+        const metronome = setInterval(() => {
+            console.log('useEffect timer click');
+            console.log('BPM', BPM);
+            if(playMode === 'play') {
+                woodAudio.play();
+            } 
+        }, getClickTime(BPM));
+
+        return () => clearInterval(metronome);
+    }, [playMode, BPM]);
+
     function incrementBPM(increment) { 
-       changeBPM(BPM + increment);
+       setBPM(BPM + increment);
     }
 
-    function handleBPMChange(event) { 
-        changeBPM(event.target.value); 
+    function handleBPMChange(event) {   
+        setBPM(event.target.value);  
     }
  
     function getClickTime(BPM) {
         return 60000 / BPM;
     }
 
-    function getBPM() {
-        return document.getElementById("BPM_input").value;
+    function play() {
+        setPlayMode('play');
     }
 
-    function isPlaying() { 
+    function stop() {
+        setPlayMode('stop');
+    }
+
+   /* function getBPM() {
+        return document.getElementById("BPM_input").value;
+    }*/
+
+   /* function isPlaying() { 
         return elementHasClass('playButton', 'selected'); 
     }
 
     function elementHasClass(elementId, className) {
         return Array.from(document.getElementById(elementId).classList).indexOf(className) !== -1;
-    }
+    }*/
 
-    function metronomeTick() {  
+    /*function metronomeTick(BPM) {  
+        console.log('metronomeTick', BPM);  
         if(isPlaying()) {
             if(!elementHasClass('muteButton', 'muted')) {
                 woodAudio.play();
             }
             
-            setTimeout(metronomeTick, getClickTime(getBPM()));
+            setTimeout(() => metronomeTick(BPM), getClickTime(getBPM()));
+            // setTimeout((context) => { metronomeTick()}, getClickTime(getBPM()), this);
         } 
-    }
+    }*/
 
-    function play() {   
+    /*function play() {   
         if(!elementHasClass('playButton', 'selected')) {
             addClass('playButton', 'selected');
             removeClass('stopButton', 'selected');
@@ -78,6 +103,7 @@ const Metronome = () => {
     }
 
     function stop() {   
+        console.log('stop');
         addClass('stopButton', 'selected');
         removeClass('playButton', 'selected');
         changePlayMode('stop'); 
@@ -91,15 +117,15 @@ const Metronome = () => {
             removeClass('muteButton', 'muted');
             woodAudio.muted = false;
         }
-    }
+    }*/
 
-    function addClass(elementId, className) {
+    /*function addClass(elementId, className) {
         document.getElementById(elementId).classList.add(className);
     }
 
     function removeClass(elementId, className) {
         document.getElementById(elementId).classList.remove(className);
-    }
+    }*/
  
     return (
         <div className="metronomeContainer"> 
@@ -111,7 +137,7 @@ const Metronome = () => {
 
 			<button onClick={play} id="playButton">Play</button>
 			<button onClick={stop} id="stopButton">Stop</button>
-            <button onClick={mute} id="muteButton">Mute</button>
+            {/*<button onClick={mute} id="muteButton">Mute</button>*/}
 		</div>
     )
 }
