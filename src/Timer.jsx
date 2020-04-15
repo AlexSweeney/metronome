cc0cdbf848204d6ea0a40ff6500c6c4deb6d0f35
Alex Sweeney <alexsweeney@live.co.uk>
2020-04-15 14:44:21 +0700

    refactor(Timer.jsx) manage time with useReducer instead of seperate useState hooks

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,89 +1,78 @@
 /*
 	input numbers
 		focus press up or down 
-			make work
-			change to reducer
-			give leading 0
+			change to reducer 
 
 		focus type
+		
+		play, stop, pause
+			seconds
+			minutes
+			hours
+
 		increment buttons -> above and below
-
-
-
 */
 
 
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useReducer} from 'react';
 
-function Timer() {
-	let [hours, setHours] = useState('00');
+function Timer() { 
+	/*let [hours, setHours] = useState('00');
 	let [minutes, setMinutes] = useState('00');
-	let [seconds, setSeconds] = useState('00');
+	let [seconds, setSeconds] = useState('00');*/
+	let initialTimeState = {hours: '00', minutes: '00', seconds: '00'};
+	const [timeState, dispatch] = useReducer(timeReducer, initialTimeState);
 
-	function addLeadingZero(number, addition) {
-		console.log('addLeadingZero', arguments);
-		console.log("Number(number)", Number(number));
-		number = Number(number) + addition;
-		console.log('number', number);
+	function addLeadingZero(number, addition) { 
+		number = Number(number) + addition; 
 		
 		if(number < 0) {
 			return '00';
-		}
-
-		if(number < 10) {
+		} else if(number < 10) {
 			return '0' + number;
+		} else {
+			return number;
 		} 
-		
-		return number;
 	}
 
-	function handleKeyDown(e) {
-		if(e.key === 'ArrowUp') { 
-			if(e.target.id === 'hourInput') {
-				setHours(addLeadingZero(hours, 1));
-			} else if (e.target.id === 'minuteInput') {
-				setMinutes(addLeadingZero(minutes, 1));
-			} else if (e.target.id === 'secondInput') {
-				setSeconds(addLeadingZero(seconds, 1));
-			}
-		} else if (e.key === 'ArrowDown') { 
-			if(e.target.id === 'hourInput') {
-				setHours(addLeadingZero(hours, -1));
-			} else if (e.target.id === 'minuteInput') {
-				setMinutes(addLeadingZero(minutes, -1));
-			} else if (e.target.id === 'secondInput') {
-				setSeconds(addLeadingZero(seconds, -1));
-			}
+	function timeReducer(timeState, action) { 
+		let {hours, minutes, seconds} = timeState; 
+
+		switch(action.target) {
+			case 'hourInput': 
+				hours = (addLeadingZero(hours, action.number));
+			 	break;
+			case 'minuteInput': 
+				minutes = (addLeadingZero(minutes, action.number));
+				break;
+			case 'secondInput':
+				seconds = (addLeadingZero(seconds, action.number)); 
+				break;
+		} 
+
+		return {hours, minutes, seconds}; 
+	}
+
+	function getNumberFromKey(key) {
+		if(key === 'ArrowUp') {
+			return 1;
+		} else if (key === 'ArrowDown') {
+			return -1;
+		} else {
+			return 0;
 		}
-	} 
-	
-	/*useEffect(() => {
-		inputs.forEach((input) => {
-		document.getElementById(input).addEventListener('keydown', (e) => {
-				console.log('input keydown');
-				console.log('e', e); 
-				console.log('e.target.id', e.target.id);
+	}
 
-				if(e.key === 'ArrowUp') { 
-					if(e.target.id === 'secondInput') {
-						console.log('seconds');
-						setSeconds(Number(seconds) + 1);
-					}
-				} else if (e.key === 'ArrowDown') { 
-					if(e.target.id === 'secondInput') {
-						console.log('seconds');
-						setSeconds(Number(seconds) - 1);
-					}
-				}
-			});
-		});
-	}, []); */
+	function handleKeyDown(e) { 
+		let number = getNumberFromKey(e.key);
+		dispatch({number, target: e.target.id}); 
+	}  
 
-	return (
+	return ( 
 		<>
-			<input id="hourInput" type="number" min="0" max="99" value={hours} onKeyDown={handleKeyDown}/>
-			<input id="minuteInput" type="number" min="0" max="60" value={minutes} onKeyDown={handleKeyDown}/>
-			<input id="secondInput" type="number" min="0" max="60" value={seconds} onKeyDown={handleKeyDown}/>
+			 <input id="hourInput" type="number" min="0" max="99" value={timeState.hours} onKeyDown={handleKeyDown}/>
+			<input id="minuteInput" type="number" min="0" max="60" value={timeState.minutes} onKeyDown={handleKeyDown}/>
+			<input id="secondInput" type="number" min="0" max="60" value={timeState.seconds} onKeyDown={handleKeyDown}/>
 		</>
 	)
 }
