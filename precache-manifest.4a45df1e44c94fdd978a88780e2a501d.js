9d7ea72d98c345c02d1cc34afda298ef0c70b64b
Alex Sweeney <alexsweeney@live.co.uk>
2021-06-28 20:08:43 +0700

    Updates

---
+++ precache-manifest.4a45df1e44c94fdd978a88780e2a501d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "299fb6b9cd1177ba223ad7fafacfd240",
+    "url": "/metronome/index.html"
+  },
+  {
+    "revision": "5a449cb638e74b725989",
+    "url": "/metronome/static/css/main.146b14b7.chunk.css"
+  },
+  {
+    "revision": "b68844cc74bc4e206292",
+    "url": "/metronome/static/js/2.5fba0ad2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/metronome/static/js/2.5fba0ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a449cb638e74b725989",
+    "url": "/metronome/static/js/main.a05e0ee4.chunk.js"
+  },
+  {
+    "revision": "b27ddf5a9deffcc4016f",
+    "url": "/metronome/static/js/runtime-main.70b3adb7.js"
+  },
+  {
+    "revision": "3fa2c1a2ac641ad8c3cf7ac8b360aba6",
+    "url": "/metronome/static/media/bark.3fa2c1a2.mp3"
+  },
+  {
+    "revision": "1020003c4273f9cfb4e04933f2f74e59",
+    "url": "/metronome/static/media/bell-ting.1020003c.mp3"
+  },
+  {
+    "revision": "050d261a9c7672e8c8b104e61f7aff99",
+    "url": "/metronome/static/media/click.050d261a.mp3"
+  },
+  {
+    "revision": "226f07c9eeae989a05c98db4cb001c59",
+    "url": "/metronome/static/media/kick.226f07c9.mp3"
+  },
+  {
+    "revision": "b6c6c54f5438fd09535f2d342e06d294",
+    "url": "/metronome/static/media/meow.b6c6c54f.mp3"
+  },
+  {
+    "revision": "867e6eced4eef265e4bc241367e7755a",
+    "url": "/metronome/static/media/snare.867e6ece.mp3"
+  },
+  {
+    "revision": "f56443f597dac75ab85be40900c752ae",
+    "url": "/metronome/static/media/wood.f56443f5.mp3"
+  }
+]);
