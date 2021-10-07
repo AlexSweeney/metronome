91c8454a53fa02db6e30ab23ee91442b1c9e7580
Alex Sweeney <alexsweeney@live.co.uk>
2021-10-07 19:28:39 +0700

    feat(SettingsViewToggle.jsx) add hover and active colors

--- src/components/SettingsViewToggle.jsx
+++ src/components/SettingsViewToggle.jsx
@@ -6,7 +6,7 @@ import './../styles/SettingsViewToggle.css';
 export default function SettingsViewToggle({handleClick}) {
 	return (
 		<div className="cog-container" id="cogContainer" onClick={handleClick}> 
-     	<FaCog size={30}/>  
+     	<FaCog size={30} className="cog"/>  
     </div>  
 	)
 }
